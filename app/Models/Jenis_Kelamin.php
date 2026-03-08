0c16311031f59b8ce421933ed1958934b3f533e4
Raani <safrianibelajarmultimedia@gmail.com>
2026-03-08 16:21:25 +0700

    Tugas Jaya

---
+++ app/Models/Jenis_Kelamin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jenis_Kelamin extends Model
+{
+  protected $fillable = ['nama_jenis_kelamin'];
+ use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PasienController;
+use App\Http\Controllers\DesaController;
+use App\Http\Controllers\Jenis_KelaminController;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use App\Http\Controllers\KontakController;
 /*
+use App\Http\Controllers\KontakController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -12,7 +17,49 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+Route::get('/', [DesaController::class, 'index'])->name('index');
+Route::post('/desa/{id}', [DesaController::class, 'store'])->name('desa.store');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/jeniskelamin', [Jenis_KelaminController::class, 'index']);
+Route::get('/jeniskelamin/1', [Jenis_KelaminController::class, 'store'])->name('jenis_kelamin.store');
+
+Route::resource('desa', DesaController::class);
+Route::resource('jenis-kelamin', Jenis_KelaminController::class);
+
+// return new class extends Migration {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('flights', function (Blueprint $table) {
+//             $table->id();
+//             $table->string('name');
+//             $table->string('airline');
+//             $table->timestamps();
+//         });
+//     }
+
+//     public function down(): void
+//     {
+//         Schema::drop('flights');
+//     }
+// };
+// route:: get('/pasien', [PasienController::class, 'index'])->name('home');
+// route:: get('/', [PasienController::class, 'index'])->name('home');
+//route:: post('/kontak', [KontakController::class, 'submit'])->name('kontak.submit');
+////route:: get('/kontak', [KontakController::class, 'form'])->name('kontak.form');
+//route:: get('/', [KontakController::class, 'home'])->name('home');
+
+//Route::get('/profile', [ProfileController::class, 'index']);
+//Route::post('/profile', [ProfileController::class, 'store']);
+//Route::get('/', function () {
+   // return view('welcome');
+//});
+
+//Route::get('/hello', function () {
+  //  return view('hello');
+//});
